0338afbb639452f717af1fb76cd3448f4d36cfad
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-12-19 01:06:01 +0200

    :sparkles: added Create new Stock page

--- src/pages/CreateNewStock/index.tsx
+++ src/pages/CreateNewStock/index.tsx
@@ -1,14 +1,66 @@
-import React from 'react';
-import Card from '../../components/Card';
+import React, {useState} from 'react';
+import {Label} from '../../components/Form/Label';
+import DatePicker from 'react-native-date-picker';
 
-import {Container} from './styles';
+import {Header} from '../../components/Header';
+import {Input} from '../../components/Input';
+import Feather from 'react-native-vector-icons/Feather';
+import {
+  AddButton,
+  ButtonText,
+  Container,
+  FormContainer,
+  Summary,
+  SummaryContainer,
+  SummaryData,
+  SummaryTitle,
+} from './styles';
+import LineSvg from '../../assets/line.svg';
+import {Button} from 'react-native';
 
-function CreateNewStock() {
+interface CreateNewStockProps {}
+
+export function CreateNewStock({}: CreateNewStockProps) {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
   return (
     <Container>
-      <Card title="Adicionar ovos" quantity={0} />
+      <Header title="Adicionar registo" />
+      <FormContainer>
+        <Label>Digite o número de ovos produzidos</Label>
+        <Input />
+        <Label>Escolha a data</Label>
+        <Label>{String(date)}</Label>
+        <Input icon={<Feather name="calendar" color="#EFE3C880" size={20} />} />
+        <Button title="Open" onPress={() => setOpen(true)} />
+        <DatePicker
+          modal
+          open={open}
+          date={date}
+          onConfirm={date => {
+            setOpen(false);
+            setDate(date);
+          }}
+          onCancel={() => {
+            setOpen(false);
+          }}
+        />
+      </FormContainer>
+      <SummaryContainer>
+        <LineSvg />
+        <Summary>
+          <SummaryTitle>Data</SummaryTitle>
+          <SummaryData>20/21/2021</SummaryData>
+        </Summary>
+        <Summary>
+          <SummaryTitle>Ovos</SummaryTitle>
+          <SummaryData>80</SummaryData>
+        </Summary>
+        <LineSvg />
+      </SummaryContainer>
+      <AddButton>
+        <ButtonText>Adicionar registo</ButtonText>
+      </AddButton>
     </Container>
   );
 }
-
-export default CreateNewStock;
--- src/pages/CreateNewStock/styles.ts
+++ src/pages/CreateNewStock/styles.ts
@@ -1,5 +1,45 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
+  background: ${({theme}) => theme.colors.background_primary};
+  flex: 1;
+  padding: 0 18px 0 18px;
+  /* justify-content: space-around; */
+`;
+export const FormContainer = styled.View``;
 
+export const SummaryContainer = styled.View`
+  margin-top: 50px;
+`;
+export const Summary = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 8px;
+  padding-top: 16px;
+`;
+export const SummaryData = styled.Text`
+  font-size: 14px;
+  color: ${({theme}) => theme.colors.white};
+`;
+export const SummaryTitle = styled.Text`
+  font-size: 14px;
+  color: ${({theme}) => theme.colors.white};
+  font-weight: 500;
+`;
+
+export const AddButton = styled.TouchableOpacity`
+  margin-top: 64px;
+  background: #efe3c8;
+  box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+  padding: 15px 50px;
+`;
+
+export const ButtonText = styled.Text`
+  align-items: center;
+  text-align: center;
+  font-weight: 600;
+  font-size: 16px;
+  color: #4a2b29;
 `;
