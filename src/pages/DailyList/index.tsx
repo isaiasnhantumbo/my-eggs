5dd287ee6e46c924529abccd9de99b05146a6a5c
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-12-22 16:38:27 +0200

    :sparkles: feat: show all registries from database

--- src/pages/DailyList/index.tsx
+++ src/pages/DailyList/index.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import {ScrollView} from 'react-native';
+/* eslint-disable arrow-parens */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-console */
+/* eslint-disable react/no-children-prop */
+import React, {useEffect, useState} from 'react';
+import {FlatList} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
-import Card from '../../components/Card';
+
 import {Header} from '../../components/Header';
 import {SmallCard} from '../../components/SmallCard';
-
+import getRealm from '../../services/realm';
+import formatDate from '../../utils/formatDate';
 import {Container, Input} from './styles';
 
-export function DailyList() {
+interface EggsProps {
+  _id: string;
+  number_of_eggs: string;
+  owner_id: string;
+  status: string;
+  date: Date;
+}
+
+export default function DailyList() {
+  const [eggsData, setEggsData] = useState<EggsProps[]>([]);
+
+  async function loadEggs() {
+    const realm = await getRealm();
+
+    const data = realm.objects('Eggs').sorted('date', true);
+
+    setEggsData(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    loadEggs();
+  }, []);
+
   return (
     <Container>
       <Header title="Todos os registros" />
@@ -16,16 +44,13 @@ export function DailyList() {
         placeholderTextColor="#EFE3C880"
         children={<Feather name="search" color="#EFE3C880" size={20} />}
       />
-      <ScrollView>
-        <SmallCard />
-        <SmallCard />
-        <SmallCard />
-        <SmallCard />
-        <SmallCard />
-        <SmallCard />
-        <SmallCard />
-        <SmallCard />
-      </ScrollView>
+      <FlatList
+        data={eggsData}
+        keyExtractor={item => String(item._id)}
+        renderItem={({item}) => (
+          <SmallCard date={formatDate(item.date)} eggs={item.number_of_eggs} />
+        )}
+      />
     </Container>
   );
 }
