c777689fee8c0797c6db35d3a85894533767274c
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-12-22 16:40:58 +0200

    :fire: deleted Input component

--- src/components/Input/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, {ReactNode} from 'react';
-
-import {InputText} from './styles';
-interface InputProps {
-  icon?: ReactNode;
-}
-export function Input({icon}: InputProps) {
-  return <InputText children={icon} />;
-}
